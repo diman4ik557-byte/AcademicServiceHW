3f735ac610731687b638b48f0b762bc61e1c47ed
DI4 <dikun06@mail.ru>
2026-02-10 16:40:31 +0300

    Add @RequiredArgsConstructor to AcademicService, removed unnecessary dependencies

--- src/main/java/by/step/service/impl/AcademicServiceImpl.java
+++ src/main/java/by/step/service/impl/AcademicServiceImpl.java
@@ -6,28 +6,21 @@ import by.step.model.Subject;
 import by.step.repository.GradeRepository;
 import by.step.repository.StudentRepository;
 import by.step.service.AcademicService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AcademicServiceImpl implements AcademicService {
 
     private final StudentRepository studentRepository;
     private final GradeRepository gradeRepository;
 
-    @Autowired
-    public AcademicServiceImpl(StudentRepository studentRepository,
-                               GradeRepository gradeRepository){
-        this.studentRepository = studentRepository;
-        this.gradeRepository = gradeRepository;
-    }
-
     @Override
     public void addStudent(Student student) {
         studentRepository.addStudent(student);
-
     }
 
     @Override
